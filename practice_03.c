338be5c2c912537b70a899d49490f4994ec6aa7c
dg <thsdimaker@gmail.com>
2020-05-15 00:36:01 +0900

    practice_03

--- practice_03.c
+++ practice_03.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int count;
-int message(int c);
-
 int main()
 {
-    printf("몇개를 출력 할 것인가요?");
+    int count;
+
+    printf("몇 개를 출력 할까요?");
     scanf("%d", &count);
-    message(count);
-
-    return 0;
-}
-
-int message(int c){
 
     for(int i=1;i<=count;i++){
-        if(i%2==0){
-            printf("%d. hello world\n", i);
+        if(count%2==0){
+            printf("%d Hello World\n", i);
         }else{
-            printf("%d. hi\n", i);
+            printf("%d Hi\n", i);
         }
     }
 
